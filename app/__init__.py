266fb1590c2b665ea7c557ed45605783de1d5372
thekevinhu <22kevinhu@gmail.com>
2024-12-13 12:39:12 -0600

    setup routes; basic db setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,50 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.secret_key = "hello"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/login")
+@app.route("/login", methods=["POST", "GET"])
 def login():
-    return
+    if request.method == "POST":
+        user = request.form["name"]
+        session["user"] = user
+        return redirect(url_for("user"))
+    else:
+        if "user" in session:
+            return redirect(url_for("user"))
+        return render_template("login.html")
     
+@app.route("/user", methods=["POST", "GET"])
+def user():
+    email = None
+    if "user" in session:
+        user = session["user"]
+        
+        if request.method == "POST":
+            email = request.form["email"]
+            session["email"] = email
+            return render_template("user.html", email=email)
+        else:
+            if "email" in session:
+                email = session["email"]
+        return render_template("user.html", email=email)
+    else:
+        return redirect(url_for("login"))
 
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    session.pop("email", None)
+    flash("You have been logged out!", "info")
+    return redirect(url_for("login"))
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
