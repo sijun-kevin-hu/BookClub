418ce1d4318caf4fabed330917852695d3cb4ba8
thekevinhu <22kevinhu@gmail.com>
2025-01-05 22:32:50 -0500

    Revert "add URLSafeTimedSerializer import for password reset functionality"
    This reverts commit 4d5b8c0c384b8a2e18a6b276b46767e3a7c7db45.

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -1,5 +1,4 @@
 from flask import Blueprint, request, jsonify
-from itsdangerous import URLSafeTimedSerializer
 from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.security import generate_password_hash, check_password_hash
 from app import db
